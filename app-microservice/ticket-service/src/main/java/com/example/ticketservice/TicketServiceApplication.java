9fee0769af648723ed75332ae65db099265ff4ee
Nezir Turhallı <nzr.trhll@gmail.com>
2022-05-10 14:05:04 +0300

    add ticket service config repo

--- app-microservice/ticket-service/src/main/java/com/example/ticketservice/TicketServiceApplication.java
+++ app-microservice/ticket-service/src/main/java/com/example/ticketservice/TicketServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.ticketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 public class TicketServiceApplication {
     public static void main(String[] args) {
